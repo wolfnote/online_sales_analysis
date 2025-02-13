dc458c2c83a8a53550124f069bf444acff3bfb68
wolfnote <zoranboris@gmail.com>
2025-02-13 09:45:40 +0100

    Added a new branch add-product-removal as well as a new method remove_product to product_manager.py.

--- product_manager.py
+++ product_manager.py
@@ -2,12 +2,22 @@ from product import Product  # Importing the Product class
 
 class ProductManager:
     """Manages a list of products."""
+    
     def __init__(self):
         self.products = []  # List to store products
 
     def add_product(self, product):
         """Adds a new product to the list."""
         self.products.append(product)
+
+    def remove_product(self, product_name):
+        """Removes a product from the list based on the given name."""
+        for product in self.products:
+            if product.name.lower() == product_name.lower():
+                self.products.remove(product)
+                print(f"Product '{product_name}' removed successfully.")
+                return
+        print(f"Product '{product_name}' not found.")
 
     def display_all_products(self):
         """Displays all products in the list."""
